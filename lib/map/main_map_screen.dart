06a28b43136120ccae8dacef507de7cdfa8665ad
Weng Sheng Hee <whee0017@student.monash.edu>
2021-03-14 18:00:52 +0800

    Add Marker and Display Marker

--- lib/map/main_map_screen.dart
+++ lib/map/main_map_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:my_food/map/addMarkerScreen.dart';
 
 class MapMainScreen extends StatefulWidget {
   @override
@@ -20,6 +21,30 @@ class _MapMainScreenState extends State<MapMainScreen> {
     zoom: 14.4746,
   );
 
+  Set<Marker> _markers = {};
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('id-1'),
+          position: LatLng(3.0649, 101.6009),
+          infoWindow: InfoWindow(
+            title: "User Information",
+            snippet: "Name: Google, Age: 18",
+
+            // onTap: (){
+            //   _showMyDialog();
+            // }
+            onTap: (){
+              // _showMyDialog();
+            }
+          ),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,10 +55,12 @@ class _MapMainScreenState extends State<MapMainScreen> {
         body: Stack(
           children: [
             GoogleMap(
+              markers: _markers,
               mapType: MapType.normal,
               myLocationButtonEnabled: true,
               initialCameraPosition: _kGooglePlex,
               onMapCreated: (GoogleMapController controller) {
+                _onMapCreated(controller);
                 _controllerGoogleMap.complete(controller);
                 newGoogleMapController = controller;
               },
@@ -42,7 +69,6 @@ class _MapMainScreenState extends State<MapMainScreen> {
               left: 0.0,
               right: 0.0,
               bottom: 0.0,
-
               child: Container(
                 height: 120.0,
                 decoration: BoxDecoration(
@@ -65,29 +91,37 @@ class _MapMainScreenState extends State<MapMainScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(5.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black54,
-                              blurRadius: 6.0,
-                              offset: Offset(0.7, 0.7),
-                              spreadRadius: 0.5,
-                            ),
-                          ],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              Icon(Icons.search, color: Colors.blueAccent),
-                              SizedBox(
-                                width: 10.0,
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AddMarkerScreen()));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black54,
+                                blurRadius: 6.0,
+                                offset: Offset(0.7, 0.7),
+                                spreadRadius: 0.5,
                               ),
-                              Text("Search Location"),
                             ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Icon(Icons.add, color: Colors.blueAccent),
+                                SizedBox(
+                                  width: 10.0,
+                                ),
+                                Text("Add Location"),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -98,5 +132,35 @@ class _MapMainScreenState extends State<MapMainScreen> {
             ),
           ],
         ));
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("User Info"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text("Name: Google"),
+                Text("Age: 18"),
+                Text("Gender: Male"),
+                Text("Notes: Vegan"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Done'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
